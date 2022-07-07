97dc691612cf1e87c5a4d4744ed143cf11ba2cd0
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-07-07 09:41:19 -0300

    refatorando tarefas

---
+++ src/views/Tarefas.vue
@@ -0,0 +1,39 @@
+<template>
+  <Formulario @aoSalvarTarefa="salvarTarefa" />
+  <div class="lista">
+    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+    <Box v-if="listaVazia">
+      Você não está muito produtivo hoje :(
+    </Box>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Formulario from '../components/Formulario.vue';
+import Tarefa from '../components/Tarefa.vue';
+import ITarefa from '../interfaces/ITarefa';
+import Box from '../components/Box.vue';
+
+export default defineComponent({
+  name: "VTarefas",
+  components: { Formulario, Tarefa, Box },
+  computed: {
+    listaVazia(): boolean {
+      return this.tarefas.length === 0
+    }
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    },
+  }
+});
+
+</script>
+
