8ecc8002747a8fd9c5fd46371f30c9a17ce98a01
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-07-07 11:27:40 -0300

    refatorando projeto e formulario add edit

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
 import { uuid } from "vue-uuid";
-import { createStore, Store } from "vuex";
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
 
 interface Estado {
   projetos:IProjeto[]
@@ -12,18 +12,24 @@ export const key: InjectionKey<Store<Estado>> = Symbol()
 export const store = createStore<Estado>({
   state:{
     projetos: [
-      {
-        id: uuid.v4(),
-        nome: 'TypeScript'
-      },
-      {
-        id: uuid.v4(),
-        nome: 'Vue'
-      },
-      {
-        id: uuid.v4(),
-        nome: 'Vuex'
-      }
+      
     ]
+  },
+  mutations: {
+    'ADICIONA_PROJETO'(state, nomeDoProjeto: string){
+      const projeto = {
+        id: uuid.v4(),
+        nome: nomeDoProjeto
+      } as IProjeto
+      state.projetos.push(projeto)
+    },
+    'ALTERA_PROJETO'(state, projeto: IProjeto){
+      const index = state.projetos.findIndex(proj => proj.id == projeto.id)
+      state.projetos[index] = projeto
+    },
   }
 })
+
+export function useStore(): Store<Estado>{
+  return vuexUseStore(key)
+}
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,31 +1,31 @@
 <template>
   <section class="projetos">
     <h1 class="title">Projetos</h1>
-    <form @submit.prevent="salvar">
-      <div class="field">
-        <label for="nomeDoProjeto" class="label">
-          nome do Projeto
-        </label>
-        <input type="text" class="input" v-model="nomeDoProjeto"
-          id="nomeDoProjeto">
-      </div>
-      <div class="field">
-        <button class="button" type="submit">
-          Salvar
-        </button>
-      </div>
-    </form>
+    <router-link to="/projetos/novo" class="button">
+      <span class="icon is-small">
+        <i class="fas fa-plus"></i>
+      </span>
+      <span>Novo projeto</span>
+    </router-link>
     <table class="table is-fullwidth">
       <thead>
         <tr>
           <th>ID</th>
           <th>Nome</th>
+          <th>Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="projeto in projetos" :key="projeto.id">
           <td>{{ projeto.id }}</td>
           <td>{{ projeto.nome }}</td>
+          <td>
+            <router-link :to="`/projetos/${projeto.id}`" class="button">
+              <span class="icon is-small">
+                <i class="fas fa-pencil-alt"></i>
+              </span>
+            </router-link>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -33,28 +33,17 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import IProjeto from '../interfaces/IProjeto'
-import { uuid } from 'vue-uuid'
+import { computed, defineComponent } from "vue";
+import {useStore} from '@/store'
 
 export default defineComponent({
   name: 'VProjetos',
-  data() {
+  
+ 
+  setup () {
+    const store = useStore()
     return {
-      nomeDoProjeto: '',
-      projetos: [
-
-      ] as IProjeto[],
-    }
-  },
-  methods: {
-    salvar() {
-      const projeto: IProjeto = {
-        nome: this.nomeDoProjeto,
-        id: uuid.v4(),
-      }
-      this.projetos.push(projeto)
-      this.nomeDoProjeto = ''
+      projetos: computed(() => store.state.projetos)
     }
   },
 })
